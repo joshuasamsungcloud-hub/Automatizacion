643d338704e2df4fbe9aae752ce1cc982d3df477
joshuasamsungcloud-hub <joshua.samsungcloud@gmail.com>
2026-09-14 22:16:57 -0300

    Add files via upload

---
+++ passwords_data.js
@@ -0,0 +1,647 @@
+const PASSWORDS_DATA = {
+  "papelera": [],
+  "conversion": [
+    {
+      "name": "JAGENBERG",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "WIS",
+          "label": "Usuario:",
+          "value": "Administrador",
+          "extra": ""
+        },
+        {
+          "section": "WIS",
+          "label": "Password:",
+          "value": "123456",
+          "extra": ""
+        },
+        {
+          "section": "PC PASABAN",
+          "label": "Usuario:",
+          "value": "Pasaban",
+          "extra": ""
+        },
+        {
+          "section": "PC PASABAN",
+          "label": "Password:",
+          "value": "651632",
+          "extra": ""
+        },
+        {
+          "section": "PC PASABAN",
+          "label": "Menú / Acceso:",
+          "value": "MKJL",
+          "extra": ""
+        },
+        {
+          "section": "PC JAGENBERG",
+          "label": "Usuario:",
+          "value": "Admin",
+          "extra": ""
+        },
+        {
+          "section": "PC JAGENBERG",
+          "label": "Password:",
+          "value": "maule2023",
+          "extra": ""
+        },
+        {
+          "section": "PC WIS PROCEMEX",
+          "label": "IP:",
+          "value": "10.51.3.143",
+          "extra": ""
+        },
+        {
+          "section": "PC WIS PROCEMEX",
+          "label": "Password:",
+          "value": "genius",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "PASABAN 1, 2 Y 6",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "PC PASABAN",
+          "label": "Usuario:",
+          "value": "Pasaban",
+          "extra": ""
+        },
+        {
+          "section": "PC PASABAN",
+          "label": "Password:",
+          "value": "651632 o oisengr",
+          "extra": ""
+        },
+        {
+          "section": "PC PASABAN",
+          "label": "Menú / Acceso:",
+          "value": "MKJL",
+          "extra": ""
+        },
+        {
+          "section": "Red Impresora Domino",
+          "label": "Usuario:",
+          "value": "PASABAN_X_DOMINO",
+          "extra": ""
+        },
+        {
+          "section": "Red Impresora Domino",
+          "label": "Password:",
+          "value": "control651632",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "PASABAN 2",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "WIS",
+          "label": "Usuario:",
+          "value": "wis",
+          "extra": ""
+        },
+        {
+          "section": "WIS",
+          "label": "Password:",
+          "value": "wis",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "PASABAN 3",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "WIS / Config. Valmet",
+          "label": "Usuario:",
+          "value": "admin",
+          "extra": ""
+        },
+        {
+          "section": "WIS / Config. Valmet",
+          "label": "Password:",
+          "value": "admin",
+          "extra": ""
+        },
+        {
+          "section": "PC Pasaban OP",
+          "label": "Usuario:",
+          "value": "Desactivar antivirus Trend Micro",
+          "extra": ""
+        },
+        {
+          "section": "PC Pasaban OP",
+          "label": "Password:",
+          "value": "2019..!!CMPC",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "PASABAN 4",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "Red scalance",
+          "label": "IP:",
+          "value": "192.168.1.100",
+          "extra": ""
+        },
+        {
+          "section": "Red scalance",
+          "label": "Usuario:",
+          "value": "pasaban2428_2193",
+          "extra": ""
+        },
+        {
+          "section": "Red scalance",
+          "label": "Password:",
+          "value": "pasaban65",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "PASABAN 5",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "PC WIS PROCEMEX",
+          "label": "IP:",
+          "value": "10.51.3.123",
+          "extra": ""
+        },
+        {
+          "section": "PC WIS PROCEMEX",
+          "label": "Password:",
+          "value": "genius",
+          "extra": ""
+        },
+        {
+          "section": "pc operador",
+          "label": "Usuario:",
+          "value": "Genius",
+          "extra": ""
+        },
+        {
+          "section": "pc operador",
+          "label": "Password:",
+          "value": "Genius",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "MSK",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "MAIN",
+          "label": "Usuario:",
+          "value": "Administrador",
+          "extra": ""
+        },
+        {
+          "section": "MAIN",
+          "label": "Password:",
+          "value": "tbe",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "METSO",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "PC Operador",
+          "label": "Usuario:",
+          "value": "Menu Wrapping",
+          "extra": ""
+        },
+        {
+          "section": "PC Operador",
+          "label": "Menú / Acceso:",
+          "value": "Ctrl+Alt+W",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "BOBINADORA",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "PC 1 y 2",
+          "label": "Usuario:",
+          "value": "Menu Winder",
+          "extra": ""
+        },
+        {
+          "section": "PC 1 y 2",
+          "label": "Menú / Acceso:",
+          "value": "927161",
+          "extra": ""
+        },
+        {
+          "section": "Sistema",
+          "label": "Usuario:",
+          "value": "Estacion Bobinadora",
+          "extra": ""
+        },
+        {
+          "section": "Sistema",
+          "label": "Password:",
+          "value": "1Oisengr2",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "POPE Y AGV MÁQUINA",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "TENDENCIAS BELLMER",
+          "label": "IP:",
+          "value": "192.168.65.200",
+          "extra": ""
+        },
+        {
+          "section": "TENDENCIAS BELLMER",
+          "label": "Usuario:",
+          "value": "NANO-PC-04\\Elektro",
+          "extra": ""
+        },
+        {
+          "section": "TENDENCIAS BELLMER",
+          "label": "Password:",
+          "value": "1842",
+          "extra": ""
+        },
+        {
+          "section": "PC N° 107",
+          "label": "Password:",
+          "value": "Cortadora",
+          "extra": ""
+        },
+        {
+          "section": "Pc costado bobinadora",
+          "label": "Usuario:",
+          "value": "Seguridad programa",
+          "extra": ""
+        },
+        {
+          "section": "Pc costado bobinadora",
+          "label": "Password:",
+          "value": "1842",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "RGD 1",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "HMI",
+          "label": "Usuario:",
+          "value": "MAINTENANCE",
+          "extra": ""
+        },
+        {
+          "section": "HMI",
+          "label": "Password:",
+          "value": "2022020",
+          "extra": ""
+        },
+        {
+          "section": "Programa TIA Portal",
+          "label": "Password:",
+          "value": "SVG_Safe",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "RGD 2",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "HMI",
+          "label": "Usuario:",
+          "value": "MAINTENANCE",
+          "extra": ""
+        },
+        {
+          "section": "HMI",
+          "label": "Password:",
+          "value": "1234",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "MELONI",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "Red Wifi",
+          "label": "Usuario:",
+          "value": "vlu_dobleju",
+          "extra": ""
+        },
+        {
+          "section": "Red Wifi",
+          "label": "Password:",
+          "value": "arsr7742",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "MARIUSHI",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "asrs_wohr_EXT2G",
+          "label": "IP:",
+          "value": "151.110.22.##",
+          "extra": ""
+        },
+        {
+          "section": "asrs_wohr_EXT2G",
+          "label": "Usuario:",
+          "value": "asrs_wohr_EXT2G",
+          "extra": ""
+        },
+        {
+          "section": "asrs_wohr_EXT2G",
+          "label": "Password:",
+          "value": "controlmaule",
+          "extra": ""
+        },
+        {
+          "section": "CONTROL_CONV",
+          "label": "Usuario:",
+          "value": "CONTROL_CONV",
+          "extra": ""
+        },
+        {
+          "section": "CONTROL_CONV",
+          "label": "Password:",
+          "value": "86349154",
+          "extra": ""
+        },
+        {
+          "section": "Red WiFi mariushi",
+          "label": "Usuario:",
+          "value": "ASRS-MARIUSHI",
+          "extra": ""
+        },
+        {
+          "section": "Red WiFi mariushi",
+          "label": "Password:",
+          "value": "controlmaule",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "WHOR",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "asrs_wohr",
+          "label": "IP:",
+          "value": "151.110.22.##",
+          "extra": ""
+        },
+        {
+          "section": "asrs_wohr",
+          "label": "Usuario:",
+          "value": "asrs_wohr",
+          "extra": ""
+        },
+        {
+          "section": "asrs_wohr",
+          "label": "Password:",
+          "value": "controlmaule",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "EQUIPOS SICK",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "Acceso General",
+          "label": "Usuario:",
+          "value": "cliente autorizado",
+          "extra": ""
+        },
+        {
+          "section": "Acceso General",
+          "label": "Password:",
+          "value": "SICKSAFE",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "CAMARAS Y CCTV",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "Camaras Hikvision Wifi",
+          "label": "Usuario:",
+          "value": "TP_Link_75F1",
+          "extra": ""
+        },
+        {
+          "section": "Camaras Hikvision Wifi",
+          "label": "Password:",
+          "value": "57793429",
+          "extra": ""
+        },
+        {
+          "section": "Camaras Hikvision",
+          "label": "Usuario:",
+          "value": "admin",
+          "extra": ""
+        },
+        {
+          "section": "Camaras Hikvision",
+          "label": "Password:",
+          "value": "control2023hik",
+          "extra": ""
+        },
+        {
+          "section": "PC sala control",
+          "label": "Password:",
+          "value": "conversion1",
+          "extra": ""
+        },
+        {
+          "section": "Camaras Acti",
+          "label": "Usuario:",
+          "value": "admin",
+          "extra": ""
+        },
+        {
+          "section": "Camaras Acti",
+          "label": "Password:",
+          "value": "password",
+          "extra": ""
+        },
+        {
+          "section": "Acti Sala control",
+          "label": "Usuario:",
+          "value": "admin",
+          "extra": ""
+        },
+        {
+          "section": "Acti Sala control",
+          "label": "Password:",
+          "value": "123456",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "ENFARDADORA",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "HMI",
+          "label": "Password:",
+          "value": "1234",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "AGV SOLVING",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "CVC600 Controlador",
+          "label": "IP:",
+          "value": "192.168.100.100",
+          "extra": ""
+        },
+        {
+          "section": "CVC600 Controlador",
+          "label": "Password:",
+          "value": "1397",
+          "extra": ""
+        },
+        {
+          "section": "Red Wifi",
+          "label": "Usuario:",
+          "value": "SOLVING-AGV",
+          "extra": ""
+        },
+        {
+          "section": "Red Wifi",
+          "label": "Password:",
+          "value": "25926439",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "REDES CONVERSIÓN",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "Red Wifi General",
+          "label": "IP:",
+          "value": "151.110.x.x",
+          "extra": ""
+        },
+        {
+          "section": "Red Wifi General",
+          "label": "Usuario:",
+          "value": "Control-COM450",
+          "extra": ""
+        },
+        {
+          "section": "Red Wifi General",
+          "label": "Password:",
+          "value": "controlmaule",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "REBOBINADORA",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "OP",
+          "label": "Usuario:",
+          "value": "Supervisor de configuracion",
+          "extra": ""
+        },
+        {
+          "section": "OP",
+          "label": "Password:",
+          "value": "725",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "RED PUENTE SEKISLI",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "crane_pm",
+          "label": "Usuario:",
+          "value": "crane_pm",
+          "extra": ""
+        },
+        {
+          "section": "crane_pm",
+          "label": "Password:",
+          "value": "crane_pm",
+          "extra": ""
+        }
+      ]
+    },
+    {
+      "name": "TUTELKAN",
+      "category": "CONVERSIÓN",
+      "entries": [
+        {
+          "section": "tutelkan",
+          "label": "IP:",
+          "value": "10.51.3.109",
+          "extra": ""
+        },
+        {
+          "section": "tutelkan",
+          "label": "Usuario:",
+          "value": "tutelkan",
+          "extra": ""
+        },
+        {
+          "section": "tutelkan",
+          "label": "Password:",
+          "value": "TTLKN3535",
+          "extra": ""
+        }
+      ]
+    }
+  ]
+};
